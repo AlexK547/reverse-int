405ea50d72078fa0fce6c90ab9d8cf0a3bf85b46
Aleksandr <66899951+AlexK547@users.noreply.github.com>
2021-02-27 23:19:59 +0400

    fix: fixed condition

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 module.exports = function reverse (n) {
-    let str = n + '';
+    let str = Math.abs(n) + '';
     let newStr = '';
     
-    if (str[0] === '-') {
-        for (let i = 1; i < str.length; i++) {
-            newStr = str[i] + newStr;
-        }
-        newStr = '-' + newStr;
-    } else {
-        for (let i = 0; i < str.length; i++) {
-            newStr = str[i] + newStr;
-        }
+    for (let i = 0; i < str.length; i++) {
+        newStr = str[i] + newStr;
     }
-    
+        
     return +newStr;
 }
