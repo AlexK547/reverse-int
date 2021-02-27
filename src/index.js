f497cdb8220b00dbc1badab5054d5b6499868804
Aleksandr <66899951+AlexK547@users.noreply.github.com>
2021-02-27 23:16:09 +0400

    feat: add condition

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 module.exports = function reverse (n) {
     let str = n + '';
     let newStr = '';
-    for (let i = 0; i < str.length; i++) {
-        newStr = str[i] + newStr;
+    
+    if (str[0] === '-') {
+        for (let i = 1; i < str.length; i++) {
+            newStr = str[i] + newStr;
+        }
+        newStr = '-' + newStr;
+    } else {
+        for (let i = 0; i < str.length; i++) {
+            newStr = str[i] + newStr;
+        }
     }
+    
     return +newStr;
 }
